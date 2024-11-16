b34c9b6b4b7156880a2c2bc828f1a273403cfb5b
Akum <nintrooss@gmail.com>
2024-11-16 18:09:33 +0200

    feat: add solution for task 30

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -550,8 +550,8 @@ function roundToSmallestInteger(/* number */) {
  * -5.9 => -5
  */
 // 30. Task
-function roundToLargestInteger(/* number */) {
-  throw new Error('Not implemented');
+function roundToLargestInteger(number) {
+  return Math.ceil(number);
 }
 
 /**
